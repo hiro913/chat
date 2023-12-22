f50daf501eb43f8f1ef80c4a7d21dcfe212971e2
hiro <hirowami@icloud.com>
2023-12-22 17:46:48 +0900

    Router

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from 'components/home'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
+    // <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/home' Component={Home}></Route>
+        </Routes>
+
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.!!
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header> */}
     </div>
+    // </Router>
   );
 }
 
